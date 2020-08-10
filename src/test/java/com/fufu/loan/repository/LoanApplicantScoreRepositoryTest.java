204dfce6c7e6d01bda521e9d19e68f2451a151a5
fuatkarakus <fuatkrks11@gmail.com>
2020-08-10 23:37:01 +0300

    improvements

--- src/test/java/com/fufu/loan/repository/LoanApplicantScoreRepositoryTest.java
+++ src/test/java/com/fufu/loan/repository/LoanApplicantScoreRepositoryTest.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataMongoTest
 public class LoanApplicantScoreRepositoryTest {
@@ -16,13 +20,23 @@ public class LoanApplicantScoreRepositoryTest {
 
     LoanApplicantScore result;
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     @BeforeEach
     void setUp() {
         result = LoanApplicantScore.builder()
                 .id("12312312312")
                 .score(300)
                 .build();
+        mongoTemplate.save(result, "LoanApplicantScore");
+    }
 
+    @Test
+    void shouldFindResult() {
+        Optional<LoanApplicantScore> optional = repository.findById("12312312312");
+        assertTrue(optional.isPresent());
+        assertEquals(optional.get().getScore(), 300);
     }
 
     @Test
