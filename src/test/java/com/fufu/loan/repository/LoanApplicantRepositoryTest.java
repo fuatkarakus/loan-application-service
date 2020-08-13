3ed6740a5a98f764a64e4e94f3f253e46f56f475
fuatkarakus <fuatkrks11@gmail.com>
2020-08-13 07:32:07 +0300

    improvements

--- src/test/java/com/fufu/loan/repository/LoanApplicantRepositoryTest.java
+++ src/test/java/com/fufu/loan/repository/LoanApplicantRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.fufu.loan.repository;
 
 import com.fufu.loan.domain.LoanApplicant;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,10 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigInteger;
+import java.util.List;
+import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @DataMongoTest
@@ -18,8 +21,8 @@ public class LoanApplicantRepositoryTest {
     @Autowired
     LoanApplicantRepository repository;
 
-    @Test
-    void whenGivenEntity_shouldSaveEntity() {
+    @BeforeEach
+    void dataSetup() {
         LoanApplicant applicant = LoanApplicant.builder()
                 .id("12312312312")
                 .name("fuat")
@@ -27,7 +30,16 @@ public class LoanApplicantRepositoryTest {
                 .monthlySalary(BigInteger.valueOf(1000))
                 .phoneNumber("5312341234").build();
 
-        assertEquals(applicant, repository.save(applicant));
+        repository.save(applicant);
+    }
+
+    @Test
+    void whenFindAll_shouldFindEntity() {
+        List<LoanApplicant> applicants = repository.findAll();
+
+        assertThat(applicants).isNotEmpty();
+        assertThat(applicants).extracting("id").allMatch(id -> Objects.equals(id, "12312312312"));
+
     }
 
 }
